7018a01c126853c887df741f2ca717ae4f463d3b
Hectop2021 <80922863+Hectop2021@users.noreply.github.com>
2025-11-05 17:32:56 +0000

    add ms data sql

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using DotNetCoreSqlDb.Models;
 
